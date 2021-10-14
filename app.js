32038cbfa516eff7bfdf8289e883eeb4ea4276f6
Florian Godfrey Link <fgodfreylink@pugetsound.edu>
2021-10-13 21:38:04 -0700

    added first functions to app.js that access the box score cells and put them in arrays. Then getScoreTotals can be called to update the runs total for home and away

--- app.js
+++ app.js
@@ -2,3 +2,46 @@
  * Scoreboard Homework
  * @author Florian Godfrey Link 
  */
+
+ var awayScores = [];
+ var homeScores = [];
+
+ function getAwayScoreArray() {
+     var score = document.querySelectorAll("td[name ^= 'awayScore_']")
+     for(i=0; i<score.length; i++) {
+         awayScores.push(score[i]);
+     }
+ }
+
+ function getHomeScoreArray() {
+     var score = document.querySelectorAll("td[name ^= 'homeScore_'")
+     for(i=0; i<score.length; i++) {
+         homeScores.push(score[i]);
+     }
+ }
+
+ function getScoreTotals() {
+     var awayTotal=0;
+     var homeTotal=0;
+     for(var i=0; i<awayScores.length; i++) {
+         awayTotal+=parseInt(awayScores[i].innerHTML);
+     }
+     for(var i=0; i<homeScores.length; i++) {
+         homeTotal+=parseInt(homeScores[i].innerHTML);
+     }
+     document.querySelector("td[name='awayTotal']").innerHTML = awayTotal;
+     document.querySelector("td[name='homeTotal']").innerHTML = homeTotal;
+ }
+
+function initializeListeners() {
+    
+}
+ getAwayScoreArray();
+ getHomeScoreArray();
+
+ awayScores[1].innerHTML = 1;
+ awayScores[2].innerHTML = 1;
+ homeScores[1].innerHTML = 3;
+ homeScores[2].innerHTML = 2;
+
+ getScoreTotals();
