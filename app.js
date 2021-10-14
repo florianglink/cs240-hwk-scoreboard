512106c6532c38f427a38a400e018651ad643b22
Florian Godfrey Link <fgodfreylink@pugetsound.edu>
2021-10-13 23:28:14 -0700

    Trying to figure out how to increment individuaal runs and total, committing so david can see my current progress

--- app.js
+++ app.js
@@ -20,28 +20,48 @@
      }
  }
 
- function getScoreTotals() {
-     var awayTotal=0;
-     var homeTotal=0;
-     for(var i=0; i<awayScores.length; i++) {
-         awayTotal+=parseInt(awayScores[i].innerHTML);
-     }
-     for(var i=0; i<homeScores.length; i++) {
-         homeTotal+=parseInt(homeScores[i].innerHTML);
-     }
-     document.querySelector("td[name='awayTotal']").innerHTML = awayTotal;
-     document.querySelector("td[name='homeTotal']").innerHTML = homeTotal;
- }
+function getTotal(values) {
+    var total=0;
+    for(var i=0; i<values.length; i++){
+        total +=parseInt(values[i].innerHTML);
+    }
+    return total;
+}
+//  function getScoreTotals() {
+//      var awayTotal=0;
+//      var homeTotal=0;
+//      for(var i=0; i<awayScores.length; i++) {
+//          awayTotal+=parseInt(awayScores[i].innerHTML);
+//      }
+//      for(var i=0; i<homeScores.length; i++) {
+//          homeTotal+=parseInt(homeScores[i].innerHTML);
+//      }
+//      document.querySelector("td[name='awayTotal']").innerHTML = awayTotal;
+//      document.querySelector("td[name='homeTotal']").innerHTML = homeTotal;
+//  }
 
 function initializeListeners() {
-    
+    document.querySelector("#addRun").addEventListener("click", function() {
+        var team = document.querySelector("#team");
+        var inning = document.querySelector("#inning");
+        if(team.value == "Away"){
+            awayScores[inning.value-1].innerHTML+=1;
+            document.querySelector("td[name='awayTotal']").innerHTML = getTotal(awayScores);
+        }
+        else if(team.value == "Home"){
+            homeScores[inning.value-1].innerHTML+=1;
+            document.querySelector("td[name='homeTotal']").innerHTML = getTotal(homeScores);
+        }
+    });
 }
  getAwayScoreArray();
  getHomeScoreArray();
 
- awayScores[1].innerHTML = 1;
- awayScores[2].innerHTML = 1;
- homeScores[1].innerHTML = 3;
- homeScores[2].innerHTML = 2;
+//  awayScores[1].innerHTML = 1;
+//  awayScores[2].innerHTML = 1;
+//  homeScores[1].innerHTML = 3;
+//  homeScores[2].innerHTML = 2;
 
- getScoreTotals();
+ getTotal(awayScores);
+ getTotal(homeScores);
+ initializeListeners();
